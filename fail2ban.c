085af3197054106231e88caabd0a3852a5cc97c7
Joar Heimonen <contact@joar.me>
2025-01-09 04:35:51 +0100

    feat: added stuff

---
+++ fail2ban.c
@@ -0,0 +1,29 @@
+/*
+    module fail2ban
+    namespace urn:lsi:params:xml:ns:yang:fail2ban
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include <unistd.h>
+#include <errno.h>
+#include <libxml/xmlstring.h>
+#include "procdefs.h"
+#include "agt.h"
+#include "agt_cb.h"
+#include "agt_commit_complete.h"
+#include "agt_timer.h"
+#include "agt_util.h"
+#include "agt_not.h"
+#include "agt_rpc.h"
+#include "dlq.h"
+#include "ncx.h"
+#include "ncxmod.h
+#include "ncxtypes.h"
+#include "status.h"
+#include "rpc.h"
+#include "val.h"
+#include "val123.h"
+#include "val_set_cplxval_obj.h"
